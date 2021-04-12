cef42967e5b2940e1eafde35b4d191030d9ddfa9
etycomputer-github <etycomputer@gmail.com>
2021-04-13 02:13:00 +1200

    🚧 Added a run method that logs a message every second.

--- app/consumer.py
+++ app/consumer.py
@@ -7,6 +7,14 @@ class Consumer:
     def __init__(self, log: logging):
         self.log = log
 
+    def run(self, process_mode):
+        try:
+            while True:
+                self.log.info('Consuming ' + process_mode)
+                sleep(1)
+        except KeyboardInterrupt:
+            self.log.info("Exiting the background task.")
+
 
 def create_consumer() -> Consumer:
     return Consumer(logging)
