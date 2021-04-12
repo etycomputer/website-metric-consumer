2b866e06bf5ae3f4e687bcad008eb18628db8f25
etycomputer-github <etycomputer@gmail.com>
2021-04-13 10:27:38 +1200

    🏗️ Added a --force-init-db action

--- manage.py
+++ manage.py
@@ -3,17 +3,20 @@
 
 import sys
 import re
-from app import consumer
+from app import consumer, database
 
 
 def print_help_options():
     print("---------------------------------------------------")
     print("| This app requires an action as input parameters.")
-    print("| More than one action can be passed, the sequence of the passed actions would be respected.")
+    print("| More than one action can be passed, "
+          "the sequence of the passed actions would be respected.")
     print("| manage.py [action] [action] ...")
     print("| Supported action types are as follows:")
     print("| help: Lists the possible action types.")
     print("| run: Started the background process.")
+    print("| --force-init-db: Initializes the database. "
+          "This action will drop the existing tables, before rebuilding them.")
     print("---------------------------------------------------")
 
 
@@ -24,7 +27,13 @@ if __name__ == "__main__":
         if re.compile(r"^help$", re.IGNORECASE).match(action):
             print_help_options()
             has_valid_action = True
-        if re.compile(r"^run$", re.IGNORECASE).match(action):
+        elif re.compile(r"^--force-init-db$", re.IGNORECASE).match(action):
+            app = database.create_producer()
+            app.drop_db()
+            app.create_db()
+            app.seed_db()
+            has_valid_action = True
+        elif re.compile(r"^run$", re.IGNORECASE).match(action):
             app = consumer.create_consumer()
             app.run('website metrics')
             has_valid_action = True
