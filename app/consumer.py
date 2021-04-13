66cc4087e833ac7bf23b92997ae45b215354598e
etycomputer-github <etycomputer@gmail.com>
2021-04-14 04:41:12 +1200

    🚀 Updated the consumer run command to receive messages from Kafka topic and store it in Postgres

--- app/consumer.py
+++ app/consumer.py
@@ -1,22 +1,34 @@
 import logging
+from app import database, kafka
 from time import sleep
 logging.basicConfig(format="%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S")
 
 
 class Consumer:
     log = None
+    pg_db = None
+    kafka_c = None
 
-    def __init__(self, log: logging):
+    def __init__(self, pg_db: database.MyPostgresDB, kafka_c: kafka.MyKafkaConsumer, log: logging):
         self.log = log
+        self.pg_db = pg_db
+        self.kafka_c = kafka_c
 
-    def run(self, process_mode):
+    def run(self):
         try:
             while True:
-                self.log.info('Consuming ' + process_mode)
-                sleep(1)
+                isSuccessful, measurements = self.kafka_c.receive_messages()
+                if isSuccessful:
+                    self.log.info("Received {} messages from Kafka".format(len(measurements)))
+                    self.pg_db.insert_measurements(measurements)
+                else:
+                    self.log.error("Error while receiving message from kafka.")
+                sleep(5)
         except KeyboardInterrupt:
             self.log.info("Exiting the background task.")
 
 
 def create_consumer() -> Consumer:
-    return Consumer(logging)
+    pg_db = database.create_postgres_db()
+    kafka_c = kafka.create_kafka_consumer()
+    return Consumer(pg_db, kafka_c, logging)
