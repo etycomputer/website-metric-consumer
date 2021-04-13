3471aa231d756517ea8c59231f06d92e35e784c9
etycomputer-github <etycomputer@gmail.com>
2021-04-14 04:44:46 +1200

    ✅ Adding integration_mode checks to the consumer test

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -1,21 +1,31 @@
 import unittest
 from tests import app_factory
+from config import integration_mode
 
 
 class MyConsumerTest(unittest.TestCase):
     app = None
 
     def setUp(self):
+        if integration_mode is False:
+            self.assertTrue(True)
+            return
         self.assertTrue(self.app is None)
         self.app = app_factory.build_production_app()
         # Make sure app is passed in correctly and has correct type
         self.assertTrue(self.app is not None)
 
     def tearDown(self):
+        if integration_mode is False:
+            self.assertTrue(True)
+            return
         # Make sure app is passed in correctly and has correct type
         self.assertTrue(self.app is not None)
 
     def test_instance_type(self):
+        if integration_mode is False:
+            self.assertTrue(True)
+            return
         self.assertTrue(isinstance(self.app, app_factory.Consumer))
 
 
